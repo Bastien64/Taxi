220a5b48fb8f9a1c4c728b3d325557424df14dc3
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2025-02-19 22:36:28 +0100

    Add files via upload

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,31 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     var popup = document.getElementById("popupReservation");
-    var closeBtn = document.getElementsByClassName("close")[0];
-    var contactBtn = document.getElementById("goToContact");
-    var body = document.body;
+    var span = document.getElementsByClassName("close")[0];
+    var contactLink = document.getElementById("contact");
 
-    // Afficher le popup et flouter l’arrière-plan
+    // Afficher le popup dès l'ouverture de la page
     popup.style.display = "block";
-    body.classList.add("blur");
 
-    // Fermer le popup et enlever le flou
-    closeBtn.onclick = function() {
+    // Fermer le popup quand on clique sur (X)
+    span.onclick = function() {
         popup.style.display = "none";
-        body.classList.remove("blur");
     }
 
     // Fermer le popup en cliquant à l'extérieur
     window.onclick = function(event) {
         if (event.target == popup) {
             popup.style.display = "none";
-            body.classList.remove("blur");
         }
     }
 
-    // Scroll vers la section "contact_scroll" au clic du bouton et enlever le flou
-    contactBtn.onclick = function() {
+    // Fermer le popup en cliquant sur le lien de réservation
+    contactLink.onclick = function() {
         popup.style.display = "none";
-        body.classList.remove("blur");
-        document.getElementById("contact_scroll").scrollIntoView({ behavior: "smooth" });
     }
 });
