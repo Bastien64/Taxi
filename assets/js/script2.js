79866613ee88d411b144749b212b5f50d9556cd4
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2025-03-13 19:26:20 +0100

    Add files via upload

---
+++ assets/js/script2.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var popup = document.getElementById("popupReservation");
+    var span = document.getElementsByClassName("close")[0];
+    var contactLink = document.getElementsByClassName("contact");
+
+    // Afficher le popup dès l'ouverture de la page
+    popup.style.display = "block";
+
+    // Fermer le popup quand on clique sur (X)
+    span.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    // Fermer le popup en cliquant à l'extérieur
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+
+    // Fermer le popup en cliquant sur le lien de réservation
+    contactLink.onclick = function() {
+        popup.style.display = "none";
+    }
+});
